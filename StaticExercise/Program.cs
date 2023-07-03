25d67f0bfc990ef3fa6594e2ba7cda28d2b2e5ff
owen mei <owenzackmei@gmail.com>
2023-07-03 15:37:08 -0400

    init commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"The boiling point of water is 100 degrees celcius, but it is also {TempConverter.CelsiusToFahrenheit(100)} degrees fahrenheit");
+            Console.WriteLine($"You can preheat the oven to 400 degrees fahrenheit, but you can also do it in {TempConverter.FahrenheitToCelsius(450)} degrees celcius");
         }
     }
 }
